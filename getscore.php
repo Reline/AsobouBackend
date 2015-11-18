1b091cb312d4792949aa469057253aaf65b4484c
Reline <nathanielreline@gmail.com>
2015-11-18 17:12:00 -0500

    Fixed syntax errors

--- getscore.php
+++ getscore.php
@@ -1,41 +1,40 @@
 <?php
 	header("Content-Type: application/json; charset=utf-8");
-	include 'common.php'
+	include 'common.php';
 
 	connectToDB();
 
-	// clean
+	//clean
 	$safe_player_digits_id = mysqli_real_escape_string($mysqli, $_GET['PlayerDigitsID']);
 	$safe_song_id = mysqli_real_escape_string($mysqli, $_GET['SongID']);
-	
+
 	if ($safe_song_id == "*") {
 		// get all scores for user
 		$get_all_scores_query = "SELECT * FROM Score WHERE PlayerDigitsID = " . $safe_player_digits_id;
 		$get_all_scores_response = mysqli_query($mysqli, $get_all_scores_query) or die(mysqli_error($mysqli));
-	
+
 		// json array
-		$scoreinfo = '[';
+		$score_info = '[';
 		while($row = mysqli_fetch_assoc($get_all_scores_response)) {
-			$scoreinfo .= json_encode($row);
-			$scoreinfo .= ",";
+			$score_info .= json_encode($row);
+			$score_info .= ",";
 		}
 		mysqli_free_result($get_all_scores_response);
 		mysqli_close($mysqli);
-	
-		$scoreinfo = trim($scoreinfo, ","); // get rid of whitespace
-		$scoreinfo .= ']';
-		echo $scoreinfo;
+		
+		$score_info = trim($score_info, ","); // get rid of whitespace
+		$score_info .= ']';
+		echo $score_info;
 	} else {
-		// get specified score for user
 		$get_score_query = "SELECT * FROM Score WHERE PlayerDigitsID = " . $safe_player_digits_id . " AND SongID = " . $safe_song_id;
-		$get_score_response = mysqli_query($mysqli, $get_score_query) or die (mysqli_error($mysqli));
-
-		$scoredata = "";
+		$get_score_response = mysqli_query($mysqli, $get_score_query) or die(mysqli_error($mysqli));
+	
 		$row = mysqli_fetch_assoc($get_score_response);
-		$scoredata .= json_encode($row);
+		$score_data = json_encode($row);
 
-		echo $scoredata;
-		mysqli_free_result($get_player_response);
+		echo $score_data;
+		mysqli_free_result($get_score_response);
 		mysqli_close($mysqli);
 	}
+
 ?>
